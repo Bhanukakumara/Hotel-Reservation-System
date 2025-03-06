31e8790e1f0045e1f715050907064a59d1dca652
Bhanukakumara <bhanukarasika@gmail.com>
2025-03-06 23:29:19 +0530

    Customer Register Form

--- src/main/java/controller/CustomerRegisterFormController.java
+++ src/main/java/controller/CustomerRegisterFormController.java
@@ -1,4 +1,32 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import model.Customer;
+
 public class CustomerRegisterFormController {
+
+    @FXML
+    private TextField txtLoyaityPoints;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhoneNumber;
+
+    @FXML
+    void btnAddOnAction(ActionEvent event) {
+        String name = txtName.getText();
+        String phoneNumber = txtPhoneNumber.getText();
+        Double lotailtyPoint = Double.parseDouble(txtLoyaityPoints.getText());
+        Customer customer = new Customer(name, phoneNumber, lotailtyPoint);
+    }
+
+    @FXML
+    void btnCancelOnAction(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/model/Customer.java
+++ src/main/java/model/Customer.java
@@ -1,4 +1,15 @@
 package model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Generated;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
+    private String name;
+    private String phoneNumber;
+    private Double lotailtyPoint;
 }
